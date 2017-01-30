77f6f38fadeae3551a319af52e8cf8cfe3d09f31
Franklin Crosby <obleo33@gmail.com>
2017-01-30 06:34:02 -0700

    Change functions over to es6

--- test/index.js
+++ test/index.js
@@ -2,9 +2,9 @@ const assert    = require('assert');
 const webdriver = require('selenium-webdriver');
 const test      = require('selenium-webdriver/testing');
 
-const Buttons   = require('../lib/buttons.js')
+const Buttons   = require('../lib/buttons.js');
 
-test.describe('input fields', function(){
+test.describe('input fields', ()=>{
   this.timeout(10000);
   let driver;
  beforeEach(()=>{
@@ -18,29 +18,29 @@ test.describe('input fields', function(){
    driver.quit();
  });
  test.it('should allow me to add a title and a todo', ()=>{
-   const title = driver.findElement({name: 'title'})
-   const task = driver.findElement({name: 'todo'})
+   const title = driver.findElement({name: 'title'});
+   const task = driver.findElement({name: 'todo'});
    title.sendKeys('this is a title').then(()=>{
-     return title.getAttribute('value')
+     return title.getAttribute('value');
    }).then((value)=>{
-     assert.equal(value, 'this is a title')
-   })
- })
+     assert.equal(value, 'this is a title');
+   });
+ });
  test.it('should save title edit on blur', ()=>{
-   const title = driver.findElement({name: 'title'})
-   const task = driver.findElement({name: 'todo'})
-   const saveBtn = driver.findElement({name:'saveBtn'})
-   title.sendKeys('this is a title')
-   task.sendKeys('this is a todo')
-   saveBtn.click()
-   const newTitle = driver.findElement({name:'todo-title'})
-   newTitle.sendKeys('edit ')
-   task.click()
+   const title = driver.findElement({name: 'title'});
+   const task = driver.findElement({name: 'todo'});
+   const saveBtn = driver.findElement({name:'saveBtn'});
+   title.sendKeys('this is a title');
+   task.sendKeys('this is a todo');
+   saveBtn.click();
+   const newTitle = driver.findElement({name:'todo-title'});
+   newTitle.sendKeys('edit ');
+   task.click();
    driver.navigate().refresh().then(()=>{
-     const newTitle = driver.findElement({name: 'todo-title'})
-     return newTitle.getText()
+     const newTitle = driver.findElement({name: 'todo-title'});
+     return newTitle.getText();
    }).then((text)=>{
-     assert.equal(text, 'edit this is a title')
-   })
- })
-})
+     assert.equal(text, 'edit this is a title');
+   });
+ });
+});
